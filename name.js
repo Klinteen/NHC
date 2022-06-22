4e7550ea4f4a6ece52b7dc838437421dfadc5aa8
Ekwugha Clinton <eclinton011@gmail.com>
2022-06-22 21:56:49 +0100

    Name, Height, Country

---
+++ name.js
@@ -0,0 +1,11 @@
+
+var name = window.prompt("Enter your name");
+document.write("Hi my name is "+name+ ", ");
+
+
+var height = window.prompt("What's your height?")
+document.write("my height is "+height+ ", ");
+
+var country = window.prompt("What's your country?")
+document.write("and my country is "+country+".");
+
